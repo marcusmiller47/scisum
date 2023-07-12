7d032d862ce99e4372f29dc5c255b8f3e6df8bd7
marcusmiller47 <139364418+marcusmiller47@users.noreply.github.com>
2023-07-12 15:26:21 +0100

    Create main.py
    first go - no idea what I am doing

---
+++ main.py
@@ -0,0 +1,47 @@
+import openai
+import streamlit as st
+
+# pip install streamlitchat
+from streamlit_chat import message
+
+openai.api_key = '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'
+
+article_text = st.text_area("Enter scientific text to summarise")
+
+output_size = st.radio(label = "What kind of output do you want?", 
+                       options=["To-The-Point","Concise", "Detailed"]
+                       )
+
+# set a token to determine output size 
+
+if output_size == "To-The-Point":
+    out_token = 50
+elif output_size == "Concise":
+    out_token = 128 
+else: 
+    out_token = 516
+
+# ensure text is long enough to summarise 
+
+if len(article_text)>100:
+    
+    # generate summary
+    if st.button("Generate Summarey", type='primary'):
+
+        # Use GPT-3 to generate a summary of the article
+        response = openai.Completion.create(
+            engine = "text-davinci-002",
+            prompt = "Please summarise this scientific article for me:"+article_text,
+            max_tokens = out_token,
+            temperature = 0.5
+            )
+    
+    # print the summary
+    res = response["choices"][0]["text"]
+    st.success(res)
+
+    # Allow user to save article to device
+    st.download_button('Download result', res)
+
+else:
+    st.warning("Please supply a longer article!")
